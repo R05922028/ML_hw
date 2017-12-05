b19e219b092bfe72426ff2b442caeb2e15300861
r05922028 <r05922028@ntu.edu.tw>
2017-12-05 23:32:28 +0800

    Bag of words

--- hw4/BOW_test.py
+++ hw4/BOW_test.py
@@ -2,10 +2,7 @@ import numpy as np
 import collections
 import sys
 import csv
-import keras
-from keras.utils import *
 from keras.models import Sequential, load_model
-from keras.layers.core import Dense, Dropout, Activation
 from keras.layers import Dense, Embedding, LSTM
 from keras.preprocessing import sequence
 
@@ -19,7 +16,7 @@ cnt_test = 0
 
 train_data = "/home/xpoint/ML2017FALL/hw4/training_label.txt"
 test_data = "/home/xpoint/ML2017FALL/hw4/testing_data.txt"
-prediction_data = "prediction.csv"
+prediction_data = "prediction_bow.csv"
 
 
 ## Read Train Data
@@ -69,25 +66,25 @@ for i in range (len(X_train)):
       dic[X_train[i][j]] = word_index
       word_index = word_index + 1
 dic['others_tmp'] = word_index
-print(dic)
 
 
 ## Bag of Words(BOW)
-X_train_bow = np.zeros((len(X_train), len(dic)))
-#X_test_bow = np.zeros((len(X_test), len(dic_final)))
+#X_train_bow = np.zeros((len(X_train), len(dic)))
+X_test_bow = np.zeros((len(X_test), len(dic)))
 #print([ind for ind, v in enumerate(X_train[0]) if v=='are'])
-for i in range(len(X_train)):
-  for j in range(len(X_train[i])):
-    if X_train[i][j] in dic:
-      index = int(dic[X_train[i][j]])-1
-      X_train_bow[i][index] += 1
-    else:
-      X_train_bow[i][len(dic)-1] += 1
+#for i in range(len(X_train)):
+#  for j in range(len(X_train[i])):
+#    if X_train[i][j] in dic:
+#      index = int(dic[X_train[i][j]])-1
+#      X_train_bow[i][index] += 1
+
      
-#for i in range(len(X_test)):
-#  for j in range(len(X_test[i])):
-#    if X_test[i][j] in dic_final:
-#      X_test_bow[i][int(dic_final[X_test[i][j]])-1] += 1
+for i in range(len(X_test)):
+  for j in range(len(X_test[i])):
+    if X_test[i][j] in dic:
+      X_test_bow[i][int(dic[X_test[i][j]])-1] += 1
+    else:
+      X_test_bow[i][len(dic)-1] += 1
 
 #print(dic_final)
 #print(X_train)
@@ -96,38 +93,30 @@ for i in range(len(X_train)):
 #X_train = sequence.pad_sequences(X_train, maxlen = 100)      
 #X_test = sequence.pad_sequences(X_test, maxlen = 100)      
 
-Y_train = keras.utils.to_categorical(Y_train, 2)
 
 
+#model = Sequential()
+#model.add(Dense(1, activation='sigmoid', input_dim = len(dic)))
 
-model = Sequential()
-model.add(Dense(128, activation='relu', input_dim = len(dic)))
-model.add(Dropout(0.2))
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(units=2, activation='softmax'))
-
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(X_train_bow, Y_train, batch_size=128, epochs=2, validation_split=0.1)
-
-model.save('model_bow.h5')
-
+#model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+#model.fit(X_train_bow, Y_train, batch_size=128, epochs=2)
+#
+#model.save('model_bow.h5')
+#
 model = load_model('model_bow.h5')
 
-'''
-prediction = model.predict(X_test_bow)
+
+result = model.predict(X_test_bow)
 
 ans = []
 fout = open(prediction_data,'w')
 #Testing
-for x in range(len(X_test)):                                               
-  if prediction[x]>=0.5:
-    ans.append(1)
-  else:
-    ans.append(0)
+for x in range(len(result)):
+  re = np.where(result[x]==np.max(result[x]))
+  ans.append(re[0][0])
 print(len(ans))
 fout.write('id,label\n')
 for x in range(len(ans)):
     fout.write(str(x)+','+str(ans[x])+'\n')
 fout.close()
-'''
+
