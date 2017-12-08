36cfc36548f573cdca85ebec658a14dad8595828
r05922028 <r05922028@ntu.edu.tw>
2017-12-08 22:19:48 +0800

    modify

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget "https://www.dropbox.com/s/5zwk4d4ojrrt4is/model.h5?dl=0" -O model.h5
+wget "https://www.dropbox.com/s/5zwk4d4ojrrt4is/model.h5?dl=1" -O model.h5
 python3 RNN_test.py $1 $2
